52a307eb41469b815a7bc6972432cb2021fe495e
Ffollereau <foller.fabrice@gmail.com>
2021-10-07 12:02:42 +0200

    Controller

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\DefaultController;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+Class DefaultController extends AbstractController 
+{
+    /**
+     * Route 
+     */
+    public function  index (){
+        return 
+    } 
+}
