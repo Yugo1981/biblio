146f4623c40da223d06dfa8d3e77f0001303ea88
Ffollereau <foller.fabrice@gmail.com>
2021-10-08 12:07:33 +0200

    Méthode dans le controlleur

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -14,5 +14,42 @@ Class DefaultController extends AbstractController
         return $this->render('DefaultController/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
-    } 
+    }
+    
+    /**
+     * Route ("/Default), name="bob")
+     */
+    public function bob () : Response {
+        return New Response("Hello World"); 
+    }
+
+    // Manipuler l'objet Request
+
+        // 1- Les paramètres de la requête
+            // A - Les paramètres contenus dans les routes 
+
+    
+    /**
+     * @Route("/actu/{id}", name="")     * 
+     */
+
+    public function voirActu ($id) : Response {
+        return new Response(" Voici l'actualité de ".$id.".");
+    }
+
+    // REPONSE & VUE
+    /**
+     * @Route("/actu/{id}", name="index_affichage")
+     */
+    public function affichage($id)
+    {
+      // On utilise le raccourci : il crée un objet Response
+      New Response;
+      // Et lui donne comme contenu le contenu du template
+      return $this->render('DefaultController/index.html.twig', array(
+        'id'  => $id,
+      ));
+    }
+
+
 }
