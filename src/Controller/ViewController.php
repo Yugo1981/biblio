d3f5564614d9316dfccf8f6191aeaf930fe26395
Ffollereau <foller.fabrice@gmail.com>
2021-10-11 16:15:05 +0200

    Exercice1

--- src/Controller/ViewController.php
+++ src/Controller/ViewController.php
@@ -6,15 +6,61 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+ /**
+     * @Route("/view", name="view")
+     */
 
 class ViewController extends AbstractController {
     /**
-     * @Route("/view", name="view")
+     * @Route("/default", name="view")
      */
 
     public function index() : Response {
         return $this->render('ViewController/index.html.twig', [
             'controller_name' => 'ViewController',
-        ]);
+        ]);        
     }
+
+    /**
+     * @Route ("/docu", name="view_docu")
+     */
+
+    public function afficher($id) {
+        return $this->render('ViewController/docu.html.twig', array(
+            'id'  => $id,
+        ));
+    }
+
+     /**
+     * @Route("/tableau", name="view_tab")
+     */
+    public function tables(): Response
+    {
+        // J'initialise mon tableau   
+        $tab = [10, 15, 18];
+
+        // J'appelle la vue TABLEAUX/TWIG
+        return $this->render('ViewController/tableau.html.twig', [
+        
+        // J'affiche Mon tableau
+    
+        'cours_name' => 'COMPOSANTE VUE',
+        'tableau' => $tab,
+        ]);
+        }
+
+    /**
+     * @Route("/afficher", name="afficher")
+     */
+
+     public function affichage() : Response {
+
+        $nom = 'Follereau';
+        $prenom = 'Fabrice';
+
+        return $this->render('ViewController/affichage.html.twig', array(
+            'nom'  => $nom, 
+            'prenom' => $prenom,
+          ));
+     }
 }
