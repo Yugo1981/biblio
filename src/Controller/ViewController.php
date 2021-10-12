b4b9a149edc03b8e6948be3158854b73500abfe3
Ffollereau <foller.fabrice@gmail.com>
2021-10-12 11:36:20 +0200

    Menu.html.twig

--- src/Controller/ViewController.php
+++ src/Controller/ViewController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
  /**
-     * @Route("/view", name="view")
+     * @Route("/view")
      */
 
 class ViewController extends AbstractController {
