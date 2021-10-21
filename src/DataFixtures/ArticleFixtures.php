29e3ff47129e35e64679bb4781aa4ff2c2e06c41
Ffollereau <foller.fabrice@gmail.com>
2021-10-21 11:12:32 +0200

    Création d'artcile en bdd et affichage

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Article;
+
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +11,18 @@ class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
 
-        $manager->flush();
+        
+        for ($i=0; $i<20 ; $i++ ) 
+        { 
+            $articles = new Article();
+            
+            $articles->setTitre(" Titre de l'article N°$i ")
+                    ->setContenu(" Contenu de l'article N° $i ")
+                    ->setDate(new \DateTime());   
+            
+            $manager->persist($articles);
+        }
+     $manager->flush();
     }
 }
