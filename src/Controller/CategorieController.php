1b22744f52a9ea2996feceda28450dafbe137227
Ffollereau <foller.fabrice@gmail.com>
2021-11-03 09:05:09 +0100

    Affichage utilisateurs fonctionnel

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -9,10 +9,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/categorie")
+ */
+
 class CategorieController extends AbstractController
 {
     /**
-     * @Route("/categorie", name="categorie")
+     * @Route("/", name="categorie")
      */
     public function index(CategorieRepository $categorieRepository): Response
     {
--- src/Controller/UtilisateursController.php
+++ src/Controller/UtilisateursController.php
@@ -11,8 +11,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
-  * @Route("/users")
-*/
+ * @Route("/users")
+ */
 class UtilisateursController extends AbstractController
 {
     /**
@@ -23,6 +23,15 @@ class UtilisateursController extends AbstractController
         return $this->render('utilisateurs/index.html.twig', [
             'utilisateurs' => $utilisateursRepository->findAll(),
         ]);
-    }    
-
+    }
+    
+      /**
+     * @Route("/{id}", name="utilisateurs_show", methods={"GET"})
+     */
+    public function show(Utilisateurs $utilisateurs): Response
+    {
+        return $this->render('utilisateurs/affichage.html.twig', [
+            'utilisateurs' => $utilisateurs,
+        ]);
+    }
 }
