89cc3a11d5ddbeb6005fbdb041f8447a70b03cc7
Yugo1981 <foller.fabrice@gmail.com>
2021-11-07 14:26:59 +0100

    Ajout UtilisateursType via le make:form en auto

---
+++ src/Form/UtilisateursType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Utilisateurs;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UtilisateursType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('noms')
+            ->add('prenoms')
+            ->add('photo')
+            ->add('date_naissance')
+            ->add('login')
+            ->add('password')
+            ->add('adresse')
+            ->add('email')
+            ->add('role')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Utilisateurs::class,
+        ]);
+    }
+}
