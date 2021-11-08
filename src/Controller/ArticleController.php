d7a4d438cc08ef1d293b4f88a37438bf5685a96f
Ffollereau <foller.fabrice@gmail.com>
2021-11-08 11:21:21 +0100

    Location fonction nouveau et show fait dans le controller location vue affichage et nouveau

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -121,5 +121,4 @@ class ArticleController extends AbstractController
             'article' => $article,
         ]);
     }
-
 }
--- src/Controller/LocationController.php
+++ src/Controller/LocationController.php
@@ -11,15 +11,57 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+
+/**
+ * @Route("/location")
+ */
+
 class LocationController extends AbstractController
 {
     /**
-     * @Route("/location", name="location")
+     * @Route("/", name="location_index")
      */
     public function index(LocationRepository $locationRepository): Response
     {
         return $this->render('location/index.html.twig', [
             'location' => $locationRepository->findAll(),
+        ]);
+    }
+
+     /**
+     * @Route("/new", name="location_new")
+    */
+    public function nouveau(Request $request, EntityManagerInterface $em) : Response
+    {    
+                $location = new Location();
+            
+                $location->setTitre(" Titre de la location");
+                $location->setDate(new \DateTime());
+                $location->setCategorie(" Contenu de la catégorie location");
+                $location->setImage(" Resume de la location");
+                $location->setDescription(" Description de la location");
+                $location->setValeur(" 5");
+                $location->setAdresse(" Adresse de la location");
+                $location->setAccessibility(" Aucune inspiration");
+                $location->setStatus(" En attente");
+                $location->setALaUne(" Au secours !!!!!!!!!!");
+            $em->persist($location);            
+        $em->flush();
+        return $this->render('location/nouveau.html.twig', [
+            'location' => $location,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="location_show", methods={"GET"})
+     */
+    public function show(Location $location): Response
+    {
+        return $this->render('location/affichage.html.twig', [
+            'id'=>$location->getId(),
+            'location' => $location,
         ]);
     }
 }
